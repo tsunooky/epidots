3c9d8b6df5e8fe5eaccc6ea43be9bf91decb2f42
tsunooky <adrien.coureau91@gmail.com>
2025-11-25 17:23:34 +0100

    fixed critical problem with wallpaper on startup

--- Config/config/scripts/change_wallpaper.sh
+++ Config/config/scripts/change_wallpaper.sh
@@ -2,16 +2,23 @@
 
 if [ -z "$1" ]; then
   echo "Usage: bg <path_to_wallpaper>"
-  return 1
+  exit 1
 fi
 if [ ! -f "$1" ]; then
   echo "Error: File not found: $1."
-  return 1
+  exit 1
 fi
+
+echo "$(basename "$1")" > ~/afs/.confs/.bg
 
 i3 workspace 11 > /dev/null
 sleep 0.2
 matugen image "$1"
 feh --bg-fill "$1"
+
+cp ~/.fehbg ~/afs/.confs/
+~/afs/.confs/install.sh
+
 sleep 0.6
 i3 workspace back_and_forth > /dev/null
+
