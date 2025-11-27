3735682efd48d5d1582370542cddf575ebb8a620
Adrien Coureau <adrien.coureau@r04p18.midlab.kb.sm.cri.epita.fr>
2025-11-27 12:10:10 +0100

    startup update

--- Config/config/scripts/change_wallpaper.sh
+++ Config/config/scripts/change_wallpaper.sh
@@ -12,13 +12,13 @@ fi
 echo "$(basename "$1")" > ~/afs/.confs/.bg
 
 i3 workspace 42 > /dev/null 2>&1
-sleep 0.2
+sleep 0.1
 matugen image "$1" > /dev/null 2>&1
 feh --bg-fill "$1"
 
 cp ~/.fehbg ~/afs/.confs/
 ~/afs/.confs/install.sh
 
-sleep 0.6
+sleep 0.5
 i3 workspace back_and_forth > /dev/null 2>&1
 
