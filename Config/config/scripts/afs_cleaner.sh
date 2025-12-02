446eceae2b85e84dbf2e725ab09e27c4f295686c
tsunooky <adrien.courea91@gmail.com>
2025-12-02 10:17:59 +0100

    fix afs cleaner and upgraded save history

--- Config/config/scripts/afs_cleaner.sh
+++ Config/config/scripts/afs_cleaner.sh
@@ -3,12 +3,11 @@
 AFS="$HOME/afs/.confs"
 CONF="$AFS/config"
 
-echo -e "\033[0;34m::\033[0m Current AFS usage:"
-du -sh "$AFS" 2>/dev/null
+bash -c 'used=$(fs quota ~/afs | cut -d'%' -f1); total=$((2*1024*1024)); echo -e "\033[0;34m::\033[0m Current AFS usage:            $used%"'
 
 echo -ne "\033[0;34m::\033[0m Removing bloatware...        "
 rm -rf "$AFS/"{bash,blerc,emacs,epiconf,jnewsrc,msmtprc,muttrc,pkgs.sh,slrnrc,thunderbird,xinitrc}
-rm -rf "$CONF/"{Windows,ble,cef_user_data,chromium,dconf,doomretro,enchant,geany,gedit,glib-2.0,gimp,GIMP,gtk-2.0,libfm,lxpanel,matplotlib,menus,mimeapps.list,mpv,pavucontrol.ini,pcmanfm,pulse,QtProject.conf,RStudio,rstudio,Syncplay,syncplay.ini,Thunar,tint2,vlc,wslu,xfce4}
+rm -rf "$CONF/"{Windows,ble,cef_user_data,chromium,dconf,doomretro,enchant,geany,gedit,glib-2.0,gimp,GIMP,gtk-2.0,libfm,lxpanel,matplotlib,menus,mimeapps.list,mpv,pavucontrol.ini,pcmanfm,pulse,QtProject.conf,RStudio,rstudio,Syncplay,syncplay.ini,Thunar,tint2,vlc,wslu,xfce4,powermenu.rasi}
 rm -rf "$CONF/Electron"
 echo -e "\033[0;32m[OK]\033[0m"
 
@@ -31,9 +30,4 @@ if [ -d "$CONF/JetBrains" ]; then
     echo -e "\033[0;32m[OK]\033[0m"
 fi
 
-echo -ne "\033[0;34m::\033[0m Removing dev artifacts...    "
-find "$HOME/afs" -type f \( -name "*.o" -o -name "*.a" -o -name "*.so" -o -name "a.out" -o -name "*.core" -o -name "vgcore.*" \) -delete
-echo -e "\033[0;32m[OK]\033[0m"
-
-echo -e "\033[0;34m::\033[0m New AFS usage:"
-du -sh "$AFS" 2>/dev/null
+bash -c 'used=$(fs quota ~/afs | cut -d'%' -f1); total=$((2*1024*1024)); echo -e "\033[0;34m::\033[0m AFS usage after update:       $used%"'
