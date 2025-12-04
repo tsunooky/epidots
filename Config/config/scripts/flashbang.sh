c80b868c07fc90169c5106ba02345e7b069850ac
tsunooky <adrien.courea91@gmail.com>
2025-12-04 11:04:11 +0100

    upgraded flashbang

--- Config/config/scripts/flashbang.sh
+++ Config/config/scripts/flashbang.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-xrandr --output DP-1 --brightness 100 > /dev/null 2>&1
-xrandr --output HDMI-1 --brightness 100 > /dev/null 2>&1
+xrandr --output DP-1 --brightness 35 > /dev/null 2>&1
+xrandr --output HDMI-1 --brightness 35 > /dev/null 2>&1
 
-sleep 0.08
+b=35
+while [ "$(echo "$b 1" | awk '{print ($1 > $2)}')" = 1 ]; do
+    xrandr --output DP-1 --brightness "$b" > /dev/null 2>&1
+    xrandr --output HDMI-1 --brightness "$b" > /dev/null 2>&1
+    b=$(echo "$b * 0.99" | bc)
+done
 
 xrandr --output DP-1 --brightness 1 > /dev/null 2>&1
 xrandr --output HDMI-1 --brightness 1 > /dev/null 2>&1
