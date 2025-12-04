8d822f80aed6065d67717cb98a108d8f0096923b
tsunooky <adrien.courea91@gmail.com>
2025-12-04 09:21:13 +0100

    improved flash

--- Config/config/scripts/flashbang.sh
+++ Config/config/scripts/flashbang.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-xrandr --output DP-1 --brightness 10 > /dev/null 2>&1
-xrandr --output HDMI-1 --brightness 10 > /dev/null 2>&1
+xrandr --output DP-1 --brightness 100 > /dev/null 2>&1
+xrandr --output HDMI-1 --brightness 100 > /dev/null 2>&1
 
-sleep 0.1
+sleep 0.08
 
 xrandr --output DP-1 --brightness 1 > /dev/null 2>&1
 xrandr --output HDMI-1 --brightness 1 > /dev/null 2>&1
