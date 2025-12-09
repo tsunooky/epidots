2a2edd868526a6aaaf36eb56f5711d7aec271dd8
darkrentin <darkrentin@gmail.com>
2025-12-09 17:44:55 +0100

    fix notebook 2

--- Config/config/scripts/note.sh
+++ Config/config/scripts/note.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 if [ ! -f "$HOME/.note.lock" ] || ! (kill -s 0 $(cat ~/.note.lock));  then
+    rm ~/.note.lock.swp
     touch ~/.note.lock
     echo $PPID > ~/.note.lock
     vim ~/afs/.note.txt +startinsert
