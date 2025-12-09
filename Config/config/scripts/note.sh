ceac24765673cea313663a2df1e970650a0c3a60
darkrentin <darkrentin@gmail.com>
2025-12-09 17:42:49 +0100

    fix notebook

--- Config/config/scripts/note.sh
+++ Config/config/scripts/note.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-if [ ! -f "$HOME/.note.lock" ];  then
+if [ ! -f "$HOME/.note.lock" ] || ! (kill -s 0 $(cat ~/.note.lock));  then
     touch ~/.note.lock
+    echo $PPID > ~/.note.lock
     vim ~/afs/.note.txt +startinsert
     rm ~/.note.lock
 fi
