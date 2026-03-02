53397eed81ef417f098f4a7fec6610a2c86cd4a0
tsunooky <adrien.coureau91@gmail.com>
2026-03-02 14:52:35 +0100

    added globals file

---
+++ Config/epidots/globals.sh
@@ -0,0 +1,34 @@
+# COLORS
+BLACK="\e[30m"
+WHITE="\e[97m"
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+GRAY="\e[90m"
+LIGHT_GRAY="\e[37m"
+LIGHT_RED="\e[91m"
+LIGHT_GREEN="\e[92m"
+LIGHT_YELLOW="\e[93m"
+LIGHT_BLUE="\e[94m"
+LIGHT_MAGENTA="\e[95m"
+LIGHT_CYAN="\e[96m"
+NC="\e[0m"
+
+# CONFIG DIRECTORIES
+AFS="$HOME/afs"
+CONFS="$AFS/.confs"
+SCRIPTS="$CONFS/epidots"
+WALLPAPERS="$CONFS/wallpapers"
+
+# KEY FILES
+DOTBG="$CONFS/.bg"
+NOTELOCK="/tmp/note.lock"
+FLASHLOCK="/tmp/flash.lock"
+
+# URLS
+REPO_EPIDOTS="https://github.com/tsunooky/epidots.git"
+REPO_WALLPAPER="https://github.com/tsunooky/epidots-wallpapers.git"
+
