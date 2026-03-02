462f7e2e625d31ddb0199eb07561106623275d79
darkrentin <darkrentin@gmail.com>
2026-03-02 16:43:52 +0100

    fix tree 2

--- confs/config/nvim/lua/plugins/lspconfig.lua
+++ confs/config/nvim/lua/plugins/lspconfig.lua
@@ -2,6 +2,15 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = { "hrsh7th/cmp-nvim-lsp" },
     config = function()
+        vim.filetype.add({
+            extension = {
+                hh = "cpp",
+                hxx = "cpp",
+                hpp = "cpp",
+                inc = "cpp",
+            },
+        })
+
         vim.diagnostic.config({
             virtual_text = true,
             signs = true,
@@ -13,11 +22,8 @@ return {
             callback = function(args)
                 local opts = { buffer = args.buf }
                 
-                -- gd: Prototype/Header (.h)
                 vim.keymap.set("n", "gd", vim.lsp.buf.declaration, opts)
-                -- gD: Implementation/Code (.c)
                 vim.keymap.set("n", "gD", vim.lsp.buf.definition, opts)
-                -- gb: Go back
                 vim.keymap.set("n", "gb", "<C-o>", opts)
                 
                 vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
@@ -29,12 +35,19 @@ return {
 
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+        local root_files = { "Makefile", "configure.ac", ".git", "compile_flags.txt", "compile_commands.json" }
+
         if vim.fn.has("nvim-0.11") == 1 then
             vim.lsp.config.clangd = {
                 cmd = { "clangd", "--background-index", "--clang-tidy", "--header-insertion=iwyu" },
                 capabilities = capabilities,
+                filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+                root_markers = root_files,
             }
-            vim.lsp.config.jdtls = { capabilities = capabilities }
+            vim.lsp.config.jdtls = { 
+                capabilities = capabilities,
+                root_markers = { "pom.xml", "gradle.build", ".git" },
+            }
             vim.lsp.enable("clangd")
             vim.lsp.enable("jdtls")
         else
@@ -42,8 +55,13 @@ return {
             lspconfig.clangd.setup({
                 cmd = { "clangd", "--background-index", "--clang-tidy", "--header-insertion=iwyu" },
                 capabilities = capabilities,
+                filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+                root_dir = lspconfig.util.root_pattern(unpack(root_files)),
             })
-            lspconfig.jdtls.setup({ capabilities = capabilities })
+            lspconfig.jdtls.setup({ 
+                capabilities = capabilities,
+                root_dir = lspconfig.util.root_pattern("pom.xml", "gradle.build", ".git"),
+            })
         end
     end
 }
