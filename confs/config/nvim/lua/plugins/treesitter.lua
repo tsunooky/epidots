16ab5c4e70438e4f6e13bfa9e813bc9670696243
darkrentin <darkrentin@gmail.com>
2026-03-03 09:57:29 +0100

    update nvim conf

--- confs/config/nvim/lua/plugins/treesitter.lua
+++ confs/config/nvim/lua/plugins/treesitter.lua
@@ -8,15 +8,15 @@ return {
   },
   config = function()
     require("neo-tree").setup({
+      close_if_last_window = true,
       filesystem = {
         filtered_items = {
           visible = true,
           hide_dotfiles = false,
           hide_gitignored = false,
           hide_by_name = {
-              ".git"
-            -- ".DS_Store",
-            -- "thumbs.db"
+              ".git",
+              ".cache"
           },
         },
         follow_current_file = { enabled = true },
