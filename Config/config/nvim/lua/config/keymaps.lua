562a39d90d418665f6c75269dc176310de17bf12
tsunooky <adrien.coureau91@gmail.com>
2026-03-09 16:02:17 +0100

    added vim keybinds to neovim

--- Config/config/nvim/lua/config/keymaps.lua
+++ Config/config/nvim/lua/config/keymaps.lua
@@ -8,6 +8,22 @@ vim.keymap.set("n", "<C-x>", "<cmd>wq<CR>", { desc = "Save and quit" })
 -- Quit without saving
 vim.keymap.set("n", "<C-q>", "<cmd>q!<CR>", { desc = "Quit without saving" })
 
+-- Formats code
+vim.keymap.set({ "n", "v" }, "<C-f>", function()
+    require("conform").format({ lsp_fallback = true })
+end, { desc = "Format file" })
+
+-- Undo / Redo
+vim.keymap.set("n", "<C-u>", "u", { desc = "Undo" })
+vim.keymap.set("i", "<C-u>", "<C-o>u", { desc = "Undo" })
+vim.keymap.set("n", "<C-y>", "<C-r>", { desc = "Redo" })
+vim.keymap.set("i", "<C-y>", "<C-o><C-r>", { desc = "Redo" })
+
+-- Copy / Paste
+vim.keymap.set("v", "<C-c>", '"+y', { desc = "Copy to clipboard" })
+vim.keymap.set("n", "<C-v>", '"+p', { desc = "Paste from clipboard" })
+vim.keymap.set("i", "<C-v>", "<C-r>+", { desc = "Paste from clipboard" })
+
 -- Tab navigation
 vim.keymap.set("n", "+", "<cmd>tabnew<CR>", { desc = "New tab" })
 vim.keymap.set("n", "<", "<cmd>tabprevious<CR>", { desc = "Previous tab" })
