a6336ba02c29592dd019249ff1c0519c59faa8ed
tsunooky <adrien.coureau91@gmail.com>
2026-09-17 00:41:01 +0200

    fix(typo)

--- Config/scripts/startup_scripts/check_update.sh
+++ Config/scripts/startup_scripts/check_update.sh
@@ -3,5 +3,5 @@
 source "$HOME/afs/.confs/scripts/globals.sh"
 
 if [ "$VERSION" -lt "$REPO_VERSION" ]; then
-    dunstify "New Epidots update available \!" "Please run update-conf\n(Current: $VERSION, Latest: $REPO_VERSION)" -t 60000
+    dunstify "New Epidots update available!" "Please run update-conf\n(Current: $VERSION, Latest: $REPO_VERSION)" -t 60000
 fi
