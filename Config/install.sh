9416be76990985c0806f947e1ee8352d560ee25d
tsunooky <adrien.coureau91@gmail.com>
2026-09-17 01:34:58 +0200

    fix(install): improved code logic

--- Config/install.sh
+++ Config/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dot_list="zshrc bashrc gitconfig config mozilla signature ssh thunderbird emacs vim vimrc Xdefaults gdbinit clang-format"
+dot_list="zshrc bashrc gitconfig gitignore config mozilla signature ssh thunderbird emacs vim vimrc Xdefaults gdbinit clang-format"
 
 for f in $dot_list; do
   rm -rf "$HOME/.$f"
